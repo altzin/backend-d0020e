73bcdca35a2af86c187d793331594c2721df87bb
pawel-dzialo <38692647+pawel-dzialo@users.noreply.github.com>
2020-03-06 10:42:10 +0100

    Add files via upload

--- src/main/java/com/example/backend/p2p/state/Link.java
+++ src/main/java/com/example/backend/p2p/state/Link.java
@@ -1,20 +1,28 @@
 package com.example.backend.p2p.state;
+import com.example.backend.p2p.time.UniformRandomStream;
+
+import java.util.Random;
 
 public class Link {
     private double weight;
     private Node source;
     private Node destination;
 
-    public Link(double weight, Node source, Node destination){
-        this.weight = weight;
-        this.source = source;
-        this.destination = destination;
-    }
+    //public Link(double weight, Node source, Node destination){
+        //this.weight = weight;
+       // this.source = source;
+        //this.destination = destination;
+  //  }
     public Link(Node source, Node destination){
-        this.weight = 1;
+        UniformRandomStream rand = new UniformRandomStream(-0.2,0.2,1234567);
+        this.weight = rand.next();
         this.source = source;
         this.destination = destination;
     }
 
     public double getWeight(){return weight;}
+    public double getNext(){
+        Random normDist = new Random();
+        return weight + normDist.nextGaussian();
+    }
 }
