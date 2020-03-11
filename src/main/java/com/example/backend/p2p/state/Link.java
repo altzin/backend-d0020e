5645dcbc43623886276e7fa139015f0a6c454077
André Christofferson <andrechristofferson@hotmail.com>
2020-03-11 10:26:57 +0100

    Update Link.java

--- src/main/java/com/example/backend/p2p/state/Link.java
+++ src/main/java/com/example/backend/p2p/state/Link.java
@@ -10,13 +10,22 @@ public class Link {
     private Node source;
     private Node destination;
 
-    //public Link(double weight, Node source, Node destination){
-        //this.weight = weight;
-       // this.source = source;
-        //this.destination = destination;
-  //  }
+    /*
+    public Link(double weight, Node source, Node destination){
+        this.weight = weight;
+        this.source = source;
+        this.destination = destination;
+    }
+     */
+
+    /**
+     * Weight is currently not in use but is planned to be implemented
+     *
+     * @param source
+     * @param destination
+     */
     public Link(Node source, Node destination){
-        this.rand = new UniformRandomStream(-0.2,0.2,1234567);
+        this.rand = new UniformRandomStream(-0.2,0.2);
         //this.weight = rand.next();
         //this.rand = new Random();
         this.source = source;
@@ -25,10 +34,12 @@ public class Link {
 
     public double getWeight(){return weight;}
 
+    /**
+     *
+     * @return next value that is added to a Nodes mAP value
+     */
     public double getNext(){
-        //Random normDist = new Random();
         return rand.next();
-
         //return rand.nextGaussian() *0.2;
     }
 }
